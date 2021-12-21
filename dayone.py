71f6e47c7e2ce7c8fd79414999ffd900a6f8eee9
Tanmay Rajgor <rajgortanmay95@gmail.com>
2021-12-21 16:35:28 -0500

    Add files via upload

---
+++ dayone.py
@@ -0,0 +1,6 @@
+print("Hello, I am here to help you decide your band name")
+print("To Decide Your Band Name, Please Go through some basic questions.")
+nameOne = input("Enter the City Name where you born :\n")
+nameTwo = input("Enter Your Pet Name : \n")
+final = nameOne + nameTwo
+print("Congratulations, Your Band Name is: " + final)
